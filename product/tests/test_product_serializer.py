7ca7d57e769cd1b4224e391e3f15d9a7c550c6a1
Luis Pavan <luihoracio@gmail.com>
2025-05-20 12:12:39 -0300

    Add order and product serializers tests, update apps and dependencies

---
+++ product/tests/test_product_serializer.py
@@ -0,0 +1,17 @@
+import pytest
+
+from product.factories import ProductFactory, CategoryFactory
+from product.serializers.product_serializer import ProductSerializer
+
+@pytest.mark.django_db
+def test_product_serializer():
+    categories = CategoryFactory.create_batch(3)
+    product = ProductFactory(category=categories)
+    serializer = ProductSerializer(product)
+    data = serializer.data
+    
+    assert data['title'] == product.title
+    assert data['price'] == product.price
+    assert data['description'] == product.description
+    assert len(data['category']) == 3
+    assert data['category'][0]['title'] == categories[0].title
