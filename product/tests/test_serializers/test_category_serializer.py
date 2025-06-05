2552086122319d02ccea3a695767a4cf4f749590
Luis Pavan <luihoracio@gmail.com>
2025-06-05 11:51:28 -0300

    chore: format code and add Docker configuration files

--- product/tests/test_serializers/test_category_serializer.py
+++ product/tests/test_serializers/test_category_serializer.py
@@ -3,18 +3,19 @@ import pytest
 from product.factories import CategoryFactory
 from product.serializers.category_serializer import CategorySerializer
 
+
 @pytest.mark.django_db
 def test_category_serializer():
     category = CategoryFactory(
-        title='Título do teste',
-        slug='teste-slug',
-        description='Descricão para o teste',
+        title="Título do teste",
+        slug="teste-slug",
+        description="Descricão para o teste",
         active=True,
     )
     serializer = CategorySerializer(category)
     data = serializer.data
-    
-    assert data['title'] == 'Título do teste'
-    assert data['slug'] == 'teste-slug'
-    assert data['description'] == 'Descricão para o teste'
-    assert data['active'] is True
+
+    assert data["title"] == "Título do teste"
+    assert data["slug"] == "teste-slug"
+    assert data["description"] == "Descricão para o teste"
+    assert data["active"] is True
